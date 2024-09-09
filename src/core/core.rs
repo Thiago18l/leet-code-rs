60a0be3618e021237c5a30c1f34a742b30f721b4
Thiago <thiago.lopes.dev@gmail.com>
2024-09-09 01:56:10 -0300

    chore: add longest common prefix resolution

--- src/core/core.rs
+++ src/core/core.rs
@@ -170,4 +170,28 @@ impl Solution {
         });
         return result;
     }
+
+    #[allow(unused)]
+    fn longest_common_prefix(strs: Vec<String>) -> String {
+      if strs.is_empty() {
+        return "".to_string();
+      }
+      let mut prefix = strs[0].clone();
+      for s in strs.iter().skip(1) {
+        while !s.starts_with(&prefix) {
+          prefix.pop();
+        }
+      }
+      prefix
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::core::core::Solution;
+
+  #[test]
+  fn test_longest_common_prefix() {
+    assert_eq!(Solution::longest_common_prefix(vec!["flower".to_string(), "flow".to_string(), "flight".to_string()]), "fl".to_string());
+  }
 }
